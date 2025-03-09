3a17d7a7623f8be3f1f25b8b1007adea0bef5075
Hoc-hh-2910 <92376544+Hoc-hh-2910@users.noreply.github.com>
2025-03-09 18:24:49 +0700

    Remove Mini_Project

--- Mini_Project1/Automotive_module_manager/function_handler.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "module_manager.h"
-#include "bitmask_ultis.h"
-
-void StatusON_Module(const struct ModuleManager *manager, ModuleID ID);
-void StatusError_Module(const struct ModuleManager *manager, ModuleID ID);
-void StatusWarning_Module(const struct ModuleManager *manager, ModuleID ID);
